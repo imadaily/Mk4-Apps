683a40ebe8e4c21f7fa264ac320a93f61d6a5941
imadaily <28007662+imadaily@users.noreply.github.com>
2018-09-03 20:44:42 +0100

    Update main.py

--- imadaily/main.py
+++ imadaily/main.py
@@ -1,1 +1,7 @@
-xxx
+"""Tryin' to app?!
+Who even knows if this'll work
+"""
+___name___         = "Tryin"
+___license___      = "MIT"
+___dependencies___ = ["app", "sleep", "wifi", "http", "ugfx_helper"]
+___categories___   = ["Sound"]
